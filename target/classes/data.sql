32cfd8db5a0cd4994dd60ec5c233704f95858fc9
Neeraj Girolkar <neeraj.girolkar@domo.com>
2017-06-02 19:15:21 +0530

    Adding updated JAR

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO subsidy (SUBSIDY_NAME, CONSUMED, AMOUNT) VALUES ('Education-Subsidy', true, 10000);
+INSERT INTO subsidy (SUBSIDY_NAME, CONSUMED, AMOUNT) VALUES ('Farmer-Subsidy', false, 20000);
